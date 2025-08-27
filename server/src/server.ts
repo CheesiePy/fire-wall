bca95a873d0c6dbfe67725ab12f5289e4ae72db5
CheesiePy <maylindenberg@gmail.com>
2025-08-27 11:59:41 +0300

    added endpoints to the server using routes/

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,23 @@
 import express , {Application, Request, Response, NextFunction} from 'express';
+import ipRoute from './routes/ips';
+import portRoute from './routes/ports';
+import ruleRoute from './routes/rules';
+import urlRoute from './routes/urls';
+
 
 const server: Application = express();
+const PORT = process.env.PORT || 5000;
+
+server.use('/api/firewall/ip', ipRoute);
+server.use('/api/firewall/port', portRoute);
+server.use('/api/firewall/rule', ruleRoute);
+server.use('/api/firewall/url', urlRoute);
 
 server.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-server.listen(5000, () => {
-  console.log('Server is running on http://localhost:5000');
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
