d9014b5a7a94dfe3c334c68bd70f942b5e4da643
CheesiePy <maylindenberg@gmail.com>
2025-08-27 17:31:01 +0300

    modified some folder structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,18 @@ import ipRoute from './routes/ips';
 import portRoute from './routes/ports';
 import ruleRoute from './routes/rules';
 import urlRoute from './routes/urls';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
+
+//
+
+
+// routes
 app.use('/api/firewall/ip', ipRoute);
 app.use('/api/firewall/port', portRoute);
 app.use('/api/firewall/rule', ruleRoute);
@@ -17,7 +24,12 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
+// error handling middleware 
 
+
+
+
+// server running
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
