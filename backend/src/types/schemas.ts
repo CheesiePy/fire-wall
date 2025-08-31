65aac9706a5fb491d9c229a0ad83f11eba924cab
CheesiePy <maylindenberg@gmail.com>
2025-08-31 19:48:59 +0300

    made sure that the ip/url/port field on the relative tables is unique

--- backend/src/types/schemas.ts
+++ backend/src/types/schemas.ts
@@ -2,7 +2,7 @@ import {pgTable, serial, text, timestamp, boolean, jsonb} from 'drizzle-orm/pg-c
 
 export const ips = pgTable('ips', {
     id: serial('id').primaryKey(),
-    ip: text('ip').notNull(),
+    ip: text('ip').notNull().unique(),
     is_blacklisted: boolean('is_blacklisted').notNull().default(false),
     is_whitelisted: boolean('is_whitelisted').notNull().default(false),
     createdAt: timestamp('created_at').defaultNow(),
@@ -11,7 +11,7 @@ export const ips = pgTable('ips', {
 
 export const ports = pgTable('ports', {
     id: serial('id').primaryKey(),
-    port: text('port').notNull(),
+    port: text('port').notNull().unique(),
     is_blacklisted: boolean('is_blacklisted').notNull().default(false),
     is_whitelisted: boolean('is_whitelisted').notNull().default(false),
     createdAt: timestamp('created_at').defaultNow(),
@@ -19,7 +19,7 @@ export const ports = pgTable('ports', {
 
 export const urls =  pgTable('urls', {
     id: serial('id').primaryKey(),
-    url: text('url').notNull(),
+    url: text('url').notNull().unique(),
     is_blacklisted: boolean('is_blacklisted').notNull().default(false),
     is_whitelisted: boolean('is_whitelisted').notNull().default(false),
     createdAt: timestamp('created_at').defaultNow(),
