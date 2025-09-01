1f63f218a19111dc63ecc09c379d2053bf131282
CheesiePy <maylindenberg@gmail.com>
2025-09-01 14:14:17 +0300

    added tests placeholders

--- backend/tests/unit/config/env.test.ts
+++ backend/tests/unit/config/env.test.ts
@@ -1,18 +1,17 @@
 // testing getEnvVariables function 
 import env from '../../../src/config/env';
+import {describe, expect, test} from '@jest/globals';
 
 describe("Environment Variables", () => {
   test("returns the correct environment variables", () => {
-    const result = env;
-    expect(result).toHaveProperty("PORT");
-    expect(result).toHaveProperty("DATABASE_URL");
-    expect(result).toHaveProperty("DB_USER");
-    expect(result).toHaveProperty("DB_PASSWORD");
-    expect(result).toHaveProperty("DB_HOST");
-    expect(result).toHaveProperty("DB_PORT");
-    expect(result).toHaveProperty("DB_NAME");
-    expect(result).toHaveProperty("ENV");
-    expect(result).toHaveProperty("DATABASE_URL");
-    expect(result).toHaveProperty("DB_CONNECTION_INTERVAL");
+    expect(env).toHaveProperty("PORT");
+    expect(env).toHaveProperty("DATABASE_URL");
+    expect(env).toHaveProperty("DB_USER");
+    expect(env).toHaveProperty("DB_PASSWORD");
+    expect(env).toHaveProperty("DB_HOST");
+    expect(env).toHaveProperty("DB_PORT");
+    expect(env).toHaveProperty("DB_NAME");
+    expect(env).toHaveProperty("ENV");
+    expect(env).toHaveProperty("DB_CONNECTION_INTERVAL");
   });
 });
---
+++ backend/tests/unit/controllers/rules.controller.test.ts
@@ -0,0 +1,7 @@
+import {describe, expect, test} from '@jest/globals';
+
+describe('Placeholder Test Suite', () => {
+  test('This is a placeholder test', () => {
+    expect(true).toBe(true);
+  });
+});
