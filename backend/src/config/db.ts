dd27478e92e1f8f6e6b7d9f9baf4582e4e3b8355
CheesiePy <maylindenberg@gmail.com>
2025-09-01 19:19:44 +0300

    made db.ts and logger.ts have a singletone pattern

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,22 +1,36 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import  env  from './env';
 import logger from './logger';
 import * as schema from '../types/schemas';
 
 
-const pool = new Pool({
-    user: env.DB_USER,
-    host: env.DB_HOST,
-    database: env.DB_NAME,
-    password: env.DB_PASSWORD,
-    port: Number(env.DB_PORT)
-});
+class Database {
+    private static instance: NodePgDatabase<typeof schema>;
+    private static pool: Pool;
 
-logger.info('Database connection pool created ✅');
+    private constructor() { }
 
-pool.on('connect', () => {
-    logger.info('Connected to the database ✅');
-});
+    public static getInstance(): NodePgDatabase<typeof schema> {
+        if (!Database.instance) {
+            Database.pool = new Pool({
+                user: env.DB_USER,
+                host: env.DB_HOST,
+                database: env.DB_NAME,
+                password: env.DB_PASSWORD,
+                port: Number(env.DB_PORT)
+            });
 
-export const db = drizzle(pool, { schema });
+            logger.info('Database connection pool created ✅');
+
+            Database.pool.on('connect', () => {
+                logger.info('Connected to the database ✅');
+            });
+            Database.instance = drizzle(Database.pool, { schema });
+        }
+        return Database.instance;
+    }
+
+}
+
+export const db = Database.getInstance();
--- backend/src/config/logger.ts
+++ backend/src/config/logger.ts
@@ -2,24 +2,40 @@ import * as winston from 'winston';
 const { format, transports } = winston;
 
 
-const logger = winston.createLogger(
-    {
-        level: 'info',
-        format: format.combine(
-            format.timestamp(),
-            format.json()
-        ),
-        transports: [
-            new transports.Console(), 
-            new transports.File({ filename: 'logs/standard.log' })
-        ],
-        exceptionHandlers: [
-            new transports.File({ filename: 'logs/exceptions.log' })
-        ],
-        rejectionHandlers: [
-            new transports.File({ filename: 'logs/rejections.log' })
-        ]
-    }
-);
 
+class Logger {
+
+    private static instance: winston.Logger;
+
+    private constructor() { }
+
+    public static getInstance(): winston.Logger {
+
+        if (!Logger.instance) {
+            Logger.instance = winston.createLogger(
+                {
+                    level: 'info',
+                    format: format.combine(
+                        format.timestamp(),
+                        format.json()
+                    ),
+                    transports: [
+                        new transports.Console(),
+                        new transports.File({ filename: 'logs/standard.log' })
+                    ],
+                    exceptionHandlers: [
+                        new transports.File({ filename: 'logs/exceptions.log' })
+                    ],
+                    rejectionHandlers: [
+                        new transports.File({ filename: 'logs/rejections.log' })
+                    ]
+                }
+            );
+        }
+        return Logger.instance;
+    }
+
+}
+
+const logger = Logger.getInstance();
 export default logger;
